5378656db89fbdd13c248d1dd4a4a793b6beb5c9
RasmusOtterlind <57478279+RasmusOtterlind@users.noreply.github.com>
2020-05-12 09:12:18 +0200

    Första push m'babies

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("IMAT 0.1");
+        primaryStage.setScene(new Scene(root, 1280, 800));
         primaryStage.show();
     }
 
